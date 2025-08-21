f0698ed39da7205b6fe4bc0d220ef3e98d119742
Nornsochetra <nornsochetra@gmail.com>
2025-08-21 13:44:23 +0700

    Initial Commit

---
+++ E-Commerce/E_CommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  E_CommerceApp.swift
+//  E-Commerce
+//
+//  Created by So Chetra on 21/8/25.
+//
+
+import SwiftUI
+
+@main
+struct E_CommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ E-CommerceTests/E_CommerceTests.swift
@@ -0,0 +1,17 @@
+//
+//  E_CommerceTests.swift
+//  E-CommerceTests
+//
+//  Created by So Chetra on 21/8/25.
+//
+
+import Testing
+@testable import E_Commerce
+
+struct E_CommerceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
